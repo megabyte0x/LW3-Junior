4b2d45700e1c57adf2e683d8bce06996f13bce34
megabyte0x <yashygkf@gmail.com>
2023-01-11 17:21:14 +0530

    ceramic-tutorial completed

---
+++ ceramic-tutorial/pages/_app.js
@@ -0,0 +1,10 @@
+import '../styles/globals.css'
+import { Provider } from "@self.id/react";
+
+export default function App({ Component, pageProps }) {
+  return (
+    <Provider client={{ ceramic: "testnet-clay" }}>
+      <Component {...pageProps} />
+    </Provider>
+  )
+}
